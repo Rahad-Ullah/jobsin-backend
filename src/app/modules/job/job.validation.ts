cf91e7c5883da6bd80e858f83d94139df5dc43ac
Rahad Ullah <rahadullah10@gmail.com>
2026-01-10 11:44:59 +0600

    refactor: job validation

--- src/app/modules/job/job.validation.ts
+++ src/app/modules/job/job.validation.ts
@@ -12,7 +12,7 @@ const notificationSettingsSchema = z
   .object({
     pushNotification: z.boolean().default(false),
     emailNotification: z.boolean().default(false),
-    repeat: z.nativeEnum(RepeatType).default(RepeatType.DAILY),
+    repeat: z.nativeEnum(RepeatType).default(RepeatType.WEEKLY),
     email: z.string().email('Email must be valid').default(''),
   })
   .strict();
