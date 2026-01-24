ffa2029be47b7695742df8bde31a22918659a0a4
Rahad Ullah <rahadullah10@gmail.com>
2026-01-24 16:33:54 +0600

    refact: get admin contact api

--- src/app/modules/contact/contact.service.ts
+++ src/app/modules/contact/contact.service.ts
@@ -1,9 +1,11 @@
+import { USER_ROLES } from '../user/user.constant';
+import { User } from '../user/user.model';
 import { IContact } from './contact.interface';
 import { Contact } from './contact.model';
 
 // ------------- create/update contact -------------
 const createUpdateContactToDB = async (
-  payload: Partial<IContact>
+  payload: Partial<IContact>,
 ): Promise<IContact> => {
   // create or update contact
   const result = await Contact.findOneAndUpdate({}, payload, {
@@ -15,8 +17,11 @@ const createUpdateContactToDB = async (
 
 // ------------- get contact -------------
 const getContactFromDB = async () => {
+  const admin = await User.findOne({ role: USER_ROLES.SUPER_ADMIN }).select(
+    '_id',
+  );
   const result = await Contact.findOne({});
-  return result;
+  return { ...result?.toObject(), adminId: admin?._id };
 };
 
 export const ContactServices = {
