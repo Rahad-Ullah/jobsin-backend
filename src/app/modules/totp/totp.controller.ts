c1f68242d2e1cd9cb9bd1df1dd1c10ac2fb9f52f
Rahad Ullah <rahadullah10@gmail.com>
2026-01-28 16:19:16 +0600

    refactor(2fa-totp): totp controller

--- src/app/modules/totp/totp.controller.ts
+++ src/app/modules/totp/totp.controller.ts
@@ -4,19 +4,19 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { TOTPService } from './totp.service';
 
-// create user
+// create 2fa token
 const createToken = catchAsync(async (req: Request, res: Response) => {
   const result = await TOTPService.generateQRCode(req.user.id);
 
   sendResponse(res, {
     success: true,
     statusCode: StatusCodes.OK,
-    message: 'User created successfully',
+    message: '2FA token created successfully',
     data: result,
   });
 });
 
-// create admin
+// verify 2fa token
 const verifyToken = catchAsync(async (req: Request, res: Response) => {
   const { userId, otp } = req.body;
   const result = await TOTPService.verifyToken(userId, otp);
@@ -24,7 +24,7 @@ const verifyToken = catchAsync(async (req: Request, res: Response) => {
   sendResponse(res, {
     success: true,
     statusCode: StatusCodes.OK,
-    message: 'Admin created successfully',
+    message: '2FA token verified successfully',
     data: result,
   });
 });
