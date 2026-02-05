e90806c222ec8ca35c5acfb00d0ee08b445c820d
Rahad Ullah <rahadullah10@gmail.com>
2026-02-05 10:13:53 +0600

    debug: limitation issue

--- src/app/modules/limitation/limitation.service.ts
+++ src/app/modules/limitation/limitation.service.ts
@@ -20,6 +20,7 @@ const getUserPlan = async (userId: string) => {
   }
 
   const sub = user.subscription as any;
+  console.log(sub);
 
   if (
     sub &&
@@ -35,6 +36,7 @@ const getUserPlan = async (userId: string) => {
 // on create job
 const onCreateJob = async (userId: string) => {
   const plan = await getUserPlan(userId);
+  console.log('-----> plan : ', plan);
 
   // check job limit for basic plan - 5 jobs per month
   if (plan === 'BASIC') {
@@ -94,6 +96,7 @@ export const onCreateAppointment = async (
   candidateId: string,
 ) => {
   const plan = await getUserPlan(userId);
+  console.log('-----> plan : ', plan);
 
   if (plan !== 'BASIC') {
     return;
